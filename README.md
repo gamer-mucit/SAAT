3919c53bcba052ada76f5e3cab5d5bae1e2828cd
Gamer Mucit <111979108+gamer-mucit@users.noreply.github.com>
2024-10-05 19:19:22 +0300

    javascript ilk ödev

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# MERHABA HOŞGELDİNİZ 
+### KODLUYORUZ_FRONTEND_101_EĞİTİMİ 'nin ilk javascript ödevine
+[PROJENİN ÖN İZLEMESİNE BURADAN ULAŞABİİLİRSİNİZ](https://)
---
+++ node.js
@@ -0,0 +1,26 @@
+
+let userName = prompt("ADINIZ NEDİR?")
+info = document.querySelector("#info")
+info.innerHTML = (`${userName ? userName : "İSİM GİRİLMEDİ ;("}`);
+
+
+function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var weekdayNumber = date.getDay();
+    var arrayOfWeekdays = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"]
+    var weekdayName = arrayOfWeekdays[weekdayNumber]
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + weekdayName;
+    document.getElementById("myClock").innerText = time;
+    document.getElementById("myClock").textContent = time;
+    setTimeout(showTime, 1000);
+}
+
+showTime();
